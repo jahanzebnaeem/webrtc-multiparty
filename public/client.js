48dfdac7fb83d671f71b12a2d93f87ddcfed9207
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-26 17:22:33 +0500

    Signaling
    Setting up signaling in the code.

--- public/client.js
+++ public/client.js
@@ -8,3 +8,46 @@ let btnRegister = document.getElementById('retister')
 let roomName
 let userName
 let participants = {}
+
+let socket = io()
+
+btnRegister.onclick = () => {
+  roomName = inputRoom.value
+  userName = inputName.value
+
+  if (roomName === '' || userName === '') {
+    alert('Room and name are required')
+  } else {
+    let message = {
+      event: 'joinRoom',
+      userName: userName,
+      roomName: roomName
+    }
+    sendMessage(message)
+    divRoomSelection.style = "display: none"
+    divMeetingRoom.style = "display: block"
+  }
+}
+
+socket.on('message', message => {
+  console.log('Message arrived', message.event)
+
+  switch(message.event) {
+    case 'newParticipantArrived':
+      receivedVideo(message.userid, message.username)
+      break
+    case 'existingParticipants':
+      onExistingParticipants(message.userid, message.existingUsers)
+      break
+    case 'receiveVideoAnswer':
+      onReceiveVideoAnswer(message.senderid, message.sdpAnswer)
+      break
+    case 'candidate':
+      addIceCandidate(message.userid, message.candidate)
+      break
+  }
+})
+
+function sendMessage(message) {
+  socket.emit('message', message)
+}
--- server.js
+++ server.js
@@ -2,12 +2,41 @@ const express = require('express')
 const app = express()
 let http = require('http').Server(app)
 let minimist = require('minimist')
+let io = require('socket.io')(http)
 
 let argv = minimist(process.argv.slice(2), {
   default: {
     as_uri: 'http//localhost:3000',
     ws_uri: 'ws://localhost:8888/kurento'
   }
+})
+
+io.on('connection', socket => {
+  socket.on('message', message => {
+    switch(message.event) {
+      case 'joinRoom':
+        joinRoom(socket, message.userName, message.roomName, err => {
+          if (err) {
+            console.log(err)
+          }
+        })
+        break
+      case 'receiveVideoFrom':
+        receiveVideoFrom(socket, message.userid, message.roomName, message.sdpOffer, err => {
+          if (err) {
+            console.log(err)
+          }
+        })
+        break
+      case 'candidate':
+        addIceCandidate(socket, message.userid, message.roomName, message.candidate, err => {
+          if (err) {
+            console.log(err)
+          }
+        })
+        break
+    }
+  })
 })
 
 app.use(express.static('public'))
