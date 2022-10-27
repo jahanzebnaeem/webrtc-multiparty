64aaf0978c75404c448b1bd8dcc0530c21c57cac
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-10-27 18:00:39 +0500

    Streams
    Sending and Receiving Streams.

--- public/client.js
+++ public/client.js
@@ -51,3 +51,127 @@ socket.on('message', message => {
 function sendMessage(message) {
   socket.emit('message', message)
 }
+
+function receiveVideo(userid, username) {
+  let video = document.createElement('video')
+  let div = document.createElement('div')
+  div.className = 'videoContainer'
+  let name = document.createElement('div')
+  video.id = userid
+  video.autoplay = true
+  name.appendChild(document.createTextNode(username))
+  div.appendChild(video)
+  div.appendChild(name)
+  divMeetingRoom.appendChild(div)
+
+  let user = {
+    id: userid,
+    username: username,
+    video: video,
+    rtcPeer: null
+  }
+
+  participants(user.id) = user
+
+  let option = {
+    remoteVideo: video,
+    onicecandidate: onIceCandidate
+  }
+
+  user.rtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, function (err) {
+    if (err) {
+      return console.error(err)
+    }
+    this.generateOffer(onOffer)
+  })
+
+  let onOffer = (err, offer, wp) => {
+    let message = {
+      event: 'receiveVideoFrom',
+      userid: user.id,
+      roomName: roomName,
+      sdpOffer: offer
+    }
+    sendMessage(message)
+  }
+
+  function onIceCandidate(candidate, wp) {
+    let message = {
+      event: 'candidate',
+      userid: user.id,
+      roomName: roomName,
+      candidate: candidate
+    }
+    sendMessage(message)
+  }
+}
+
+function onExistingParticipants(userid, existingUsers) {
+  let video = document.createElement('video')
+  let div = document.createElement('div')
+  div.className = 'videoContainer'
+  let name = document.createElement('div')
+  video.id = userid
+  video.autoplay = true
+  name.appendChild(document.createTextNode(userName))
+  div.appendChild(video)
+  div.appendChild(name)
+  divMeetingRoom.appendChild(div)
+
+  let user = {
+    id: userid,
+    username: userName,
+    video: video,
+    rtcPeer: null
+  }
+
+  participants(user.id) = user
+
+  let constraints = {
+    audio: true,
+    video: {
+      mandatory: {
+        maxWidth: 320,
+        maxFrameRate: 15,
+        minFrameRate: 15
+      }
+    }
+  }
+
+  let option = {
+    localVideo: video,
+    onicecandidate: onIceCandidate,
+    mediaConstraints: constraints
+  }
+
+  user.rtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(options, function (err) {
+    if (err) {
+      return console.error(err)
+    }
+    this.generateOffer(onOffer)
+  })
+
+  existingUsers.forEach(element => {
+    receiveVideo(element.id, element.name)
+  })
+
+  let onOffer = (err, offer, wp) => {
+    let message = {
+      event: 'receiveVideoFrom',
+      userid: user.id,
+      roomName: roomName,
+      sdpOffer: offer
+    }
+    sendMessage(message)
+  }
+
+  function onIceCandidate(candidate, wp) {
+    let message = {
+      event: 'candidate',
+      userid: user.id,
+      roomName: roomName,
+      candidate: candidate
+    }
+    sendMessage(message)
+  }
+}
